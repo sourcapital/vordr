927d9ed99c5eedfbac9de6a007a4dafa9709f700
rekt0x <mail@rekt0x.com>
2022-10-01 19:18:10 +0800

    Fix: Do some minor comment editing

--- src/main.ts
+++ src/main.ts
@@ -11,65 +11,20 @@ global.log = new Log(
 )
 
 /*
-const node = new Bitcoin(
-    'https://bitcoin.ninerealms.com',
-    8332,
-    'thorchain',
-    'password'
-)
+const node = new Bitcoin('https://bitcoin.ninerealms.com', 8332, 'thorchain', 'password')
+const node = new Bitcoin('https://litecoin.ninerealms.com', 9332, 'thorchain', 'password', Fork.Litecoin)
+const node = new Bitcoin('https://bitcoin-cash.ninerealms.com', 8332, 'thorchain', 'password', Fork.BitcoinCash)
+const node = new Bitcoin('https://dogecoin.ninerealms.com', 22555, 'thorchain', 'password', Fork.Dogecoin)
 
-const node = new Bitcoin(
-    'https://litecoin.ninerealms.com',
-    9332,
-    'thorchain',
-    'password',
-    Fork.Litecoin
-)
+const node = new Ethereum('https://ethereum.ninerealms.com', 8545)
+const node = new Ethereum('https://avalanche.ninerealms.com/ext/bc/C/rpc', 9650, EvmChain.Avalanche)
 
-const node = new Bitcoin(
-    'https://bitcoin-cash.ninerealms.com',
-    8332,
-    'thorchain',
-    'password',
-    Fork.BitcoinCash
-)
-
-const node = new Bitcoin(
-    'https://dogecoin.ninerealms.com',
-    22555,
-    'thorchain',
-    'password',
-    Fork.Dogecoin
-)
-
-const node = new Ethereum(
-    'https://ethereum.ninerealms.com',
-    8545
-)
-
-const node = new Ethereum(
-    'https://avalanche.ninerealms.com/ext/bc/C/rpc',
-    9650,
-    EvmChain.Avalanche
-)
-
-const node = new Cosmos(
-    'https://gaia.ninerealms.com',
-    26657
-)
-
-const node = new Cosmos(
-    'https://binance.ninerealms.com',
-    27147,
-    AppChain.Binance
-)
+const node = new Cosmos('https://gaia.ninerealms.com', 26657)
+const node = new Cosmos('https://binance.ninerealms.com', 27147, AppChain.Binance)
+const node = new Cosmos('https://rpc.ninerealms.com', 27147, AppChain.Thorchain)
 */
 
-const node = new Cosmos(
-    'https://rpc.ninerealms.com',
-    27147,
-    AppChain.Thorchain
-)
+const node = new Cosmos('https://rpc.ninerealms.com', 27147, AppChain.Thorchain)
 
 const isUp = await node.isUp()
 const isSynced = await node.isSynced()
