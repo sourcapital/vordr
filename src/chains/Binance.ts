e7ced07b1542630d595b36de9261668b20fe22fa
rekt0x <mail@rekt0x.com>
2022-10-06 12:21:52 +0800

    Fix: Reduce delay block down to 1 for Binance

--- src/chains/Binance.ts
+++ src/chains/Binance.ts
@@ -2,6 +2,6 @@ import {Cosmos, Chain} from './Cosmos.js'
 
 export class Binance extends Cosmos {
     constructor(url: string) {
-        super(url, 3, Chain.Binance)
+        super(url, 1, Chain.Binance)
     }
 }
