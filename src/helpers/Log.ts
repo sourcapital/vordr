fe125d6b675af11a4c0d403cb647d2e20ac0096a
rekt0x <mail@rekt0x.com>
2023-06-22 19:50:11 +0800

    Do not print log messages

--- src/helpers/Log.ts
+++ src/helpers/Log.ts
@@ -9,32 +9,24 @@ export class Log {
     }
 
     async debug(message: string) {
-        console.debug(message)
-
         if (config.nodeENV === 'production') {
             await this.log?.debug(message)
         }
     }
 
     async info(message: string) {
-        console.info(message)
-
         if (config.nodeENV === 'production') {
             await this.log?.info(message)
         }
     }
 
     async warn(message: string) {
-        console.warn(message)
-
         if (config.nodeENV === 'production') {
             await this.log?.warn(message)
         }
     }
 
     async error(message: string, raise: boolean = false) {
-        console.error(message)
-
         if (config.nodeENV === 'production') {
             await this.log?.error(message)
         }
