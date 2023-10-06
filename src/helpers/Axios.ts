57b410824e97bfbfcd9cb8ce1bdff9fb407228f2
rekt0x <mail@rekt0x.com>
2023-10-06 10:24:09 +0800

    Intercept Axios errors and log more verbose

--- src/helpers/Axios.ts
+++ src/helpers/Axios.ts
@@ -1,5 +1,23 @@
 import axios, {AxiosResponse} from 'axios'
 
+axios.interceptors.response.use(
+    (response) => {
+        return response
+    },
+    async (error) => {
+        if (error.response) {
+            await log.error(`
+                Error occurred for URL: ${error.config.url}\n
+                Error status: ${error.response.status}\n
+                Error headers: ${JSON.stringify(error.response.headers, null, 2)}
+            `)
+        } else {
+            await log.error(`Error: ${error.message}`)
+        }
+        return Promise.reject(error)
+    }
+)
+
 export const safeAxiosGet = async (url: string): Promise<AxiosResponse | undefined> => {
     try {
         return await axios.get(url)
